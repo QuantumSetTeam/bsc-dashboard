d4c7499e975c1d772c5c96236c3484c79017b7d9
JimmyFxx <jimmy>
2021-04-08 23:55:12 -0300

    fixes for voting

--- src/constants/tokens.js
+++ src/constants/tokens.js
@@ -1,37 +1,72 @@
-import { UniV2PairAddress, GovernanceToken, Dao, Dai, Dollar, PoolBonding } from './contracts';
+import {
+    UniV2PairAddress,
+    GovernanceToken,
+    Dao,
+    Dai,
+    Dollar,
+    PoolBonding,
+    PoolLP,
+    PoolGov,
+    // newPoolLP,
+    // newPoolBonding,
+} from './contracts';
 
 export const UNI = {
-  addr: UniV2PairAddress,
-  decimals: 18,
-  symbol: 'UNI',
+    addr: UniV2PairAddress,
+    decimals: 18,
+    symbol: 'UNI',
 };
 
 export const BUSD = {
-  addr: Dai,
-  decimals: 18,
-  symbol: 'BUSD',
+    addr: Dai,
+    decimals: 18,
+    symbol: 'BUSD',
 };
 
 export const QSD = {
-  addr: Dollar,
-  decimals: 18,
-  symbol: 'QSD',
+    addr: Dollar,
+    decimals: 18,
+    symbol: 'QSD',
 };
 
 export const QSDS = {
-  addr: Dao,
-  decimals: 18,
-  symbol: 'QSDS',
+    addr: Dao,
+    decimals: 18,
+    symbol: 'QSDS',
 };
 
 export const QSG = {
-  addr: GovernanceToken,
-  decimals: 18,
-  symbol: 'QSG',
+    addr: GovernanceToken,
+    decimals: 18,
+    symbol: 'QSG',
 };
 
 export const PoolBondingAdd = {
-  addr: PoolBonding,
-  decimals: 18,
-  symbol: 'PoolBonding',
+    addr: PoolBonding,
+    decimals: 18,
+    symbol: 'PoolBonding',
+};
+
+export const PoolLPAdd = {
+    addr: PoolLP,
+    decimals: 18,
+    symbol: 'PoolLP',
+};
+
+// export const newPoolBondingAdd = {
+//     addr: newPoolBonding,
+//     decimals: 18,
+//     symbol: 'PoolBonding',
+// };
+
+// export const newPoolLPAdd = {
+//     addr: newPoolLP,
+//     decimals: 18,
+//     symbol: 'PoolLP',
+// };
+
+export const PoolGovAdd = {
+    addr: PoolGov,
+    decimals: 18,
+    symbol: 'PoolGov',
 };
